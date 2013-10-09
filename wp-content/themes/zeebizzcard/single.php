afe71228aa1f51aac49b99aa3a5a488e10c12db3
kidonchu <kidonchu@gmail.com>
2013-10-08 23:41:17 -0500

    Add breadcrumbs to single post and resume

--- wp-content/themes/zeebizzcard/single.php
+++ wp-content/themes/zeebizzcard/single.php
@@ -2,6 +2,10 @@
 
 <div class="content">
 
+	<?php if (function_exists('yoast_breadcrumb')) {
+		yoast_breadcrumb('<div class="breadcrumbs">','</div>');
+	} ?>
+
 	<?php if (have_posts()) : while (have_posts()) : the_post();
 
 		get_template_part( 'loop', 'single' );
